174a1952bf9eb3a271cdfe44f0d9d4962043d165
Viktar Davidovich <viktor.davidovich@gmail.com>
2024-09-30 18:42:11 -0700

    feat: moved resources into separate files, added iam resource

--- task_1/main.tf
+++ task_1/main.tf
@@ -23,8 +23,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "temp_bucket" {
-  bucket = "my-temp-terraform-state-bucket"
-}
+
 
 
---
+++ task_1/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "temp_bucket" {
+  bucket = "my-temp-terraform-bucket"
+}
